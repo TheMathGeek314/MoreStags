92ec75a8bda3c57d98aedcced619a59789c8bf17
nerthul11 <leandrorositto92@gmail.com>
2025-10-16 07:30:28 -0300

    Added logic workflow for connections

--- Rando/LogicAdder.cs
+++ Rando/LogicAdder.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using Newtonsoft.Json;
 using RandomizerCore.Json;
 using RandomizerCore.Logic;
 using RandomizerCore.LogicItems;
@@ -8,14 +10,17 @@ using RandomizerMod.RC;
 using RandomizerMod.Settings;
 
 namespace MoreStags {
-    public static class LogicAdder {
-        public static void Hook() {
+    public static class LogicAdder
+    {
+        public static void Hook()
+        {
             RCData.RuntimeLogicOverride.Subscribe(50, ApplyLogic);
-            RCData.RuntimeLogicOverride.Subscribe(1000.3f, TryMakeSubstitutions);
+            RCData.RuntimeLogicOverride.Subscribe(69420f, AlterConnectionLogic);
         }
 
-        private static void ApplyLogic(GenerationSettings gs, LogicManagerBuilder lmb) {
-            if(!MoreStags.Settings.Enabled)
+        private static void ApplyLogic(GenerationSettings gs, LogicManagerBuilder lmb)
+        {
+            if (!MoreStags.Settings.Enabled)
                 return;
             JsonLogicFormat fmt = new();
             Assembly assembly = typeof(LogicAdder).Assembly;
@@ -32,21 +37,52 @@ namespace MoreStags {
             DefineTermsAndItems(lmb, fmt);
         }
 
-        private static void DefineTermsAndItems(LogicManagerBuilder lmb, JsonLogicFormat fmt) {
+        private static void DefineTermsAndItems(LogicManagerBuilder lmb, JsonLogicFormat fmt)
+        {
             using Stream t = typeof(LogicAdder).Assembly.GetManifestResourceStream("MoreStags.Resources.terms.json");
             lmb.DeserializeFile(LogicFileType.Terms, fmt, t);
 
-            foreach(string stagName in StagData.allStags.Where(stag => !stag.isVanilla).Select(stag => RandoInterop.nameToLocation(stag.name))) {
+            foreach (string stagName in StagData.allStags.Where(stag => !stag.isVanilla).Select(stag => RandoInterop.nameToLocation(stag.name)))
+            {
                 lmb.AddItem(new SingleItem(stagName, new RandomizerCore.TermValue(lmb.GetTerm(stagName), 1)));
             }
         }
 
-        private static void TryMakeSubstitutions(GenerationSettings gs, LogicManagerBuilder lmb) {
-            if(!MoreStags.Settings.Enabled)
+        private static void AlterConnectionLogic(GenerationSettings gs, LogicManagerBuilder lmb)
+        {
+            if (!MoreStags.Settings.Enabled)
                 return;
-            JsonLogicFormat fmt = new();
-            using Stream s = typeof(LogicAdder).Assembly.GetManifestResourceStream("MoreStags.Resources.logicSubstitutions.json");
-            lmb.DeserializeFile(LogicFileType.LogicSubst, fmt, s);
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            JsonSerializer jsonSerializer = new() { TypeNameHandling = TypeNameHandling.Auto };
+
+            using Stream stream = assembly.GetManifestResourceStream("BreakableWallRandomizer.Resources.Logic.ConnectionOverrides.json");
+            StreamReader reader = new(stream);
+            List<ConnectionLogicObject> objectList = jsonSerializer.Deserialize<List<ConnectionLogicObject>>(new JsonTextReader(reader));
+
+            foreach (ConnectionLogicObject o in objectList)
+            {
+                foreach (var sub in o.logicSubstitutions)
+                {
+                    bool exists = lmb.LogicLookup.TryGetValue(o.name, out _);
+                    if (exists)
+                        lmb.DoSubst(new(o.name, sub.Key, sub.Value));
+                }
+
+                if (o.logicOverride != "")
+                {
+                    bool exists = lmb.LogicLookup.TryGetValue(o.name, out _);
+                    if (exists)
+                        lmb.DoLogicEdit(new(o.name, o.logicOverride));
+                }
+            }
         }
+    }
+    
+    public class ConnectionLogicObject
+    {
+        public string name;
+        public string logicOverride;
+        public Dictionary<string, string> logicSubstitutions;
     }
 }
